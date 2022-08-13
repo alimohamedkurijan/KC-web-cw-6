b2dd26355142ea901b47d17cb06a49cd15bc69f4
alimohamedkurijan <109865837+alimohamedkurijan@users.noreply.github.com>
2022-08-13 04:06:48 -0700

    some changes

--- ali mohammed/calc.js
+++ ali mohammed/calc.js
@@ -1,33 +1,34 @@
 alert(`مرحبا بك في موقعي الذي سيحسب درجنك`)
 
 
-let  grade =  prompt(`ادخل درجتك هنا`);
 
+let  grade =  prompt(`ادخل درجتك هنا`);
+console.log(grade)
 
 if (  grade > 90 &&  grade < 100 ){
 
 
 console.log(`لقد حصلت على امتياز 🥳`)
 
-} else if( grade > 80 && grade <89 ){
+} else if( grade > 80 && grade < 89 ){
 
 
 console.log(`لقد حصلت على جيد جداً🤩`)
 
-} else if ( grade >70 && grade < 79){
+} else if ( grade > 70 && grade < 79){
 
 console.log(`لقد حصلت على امتياز 🥳`)
 
 
-}else if ( grade >60 && grade< 69){
+}else if ( grade >60 && grade < 69){
 
     console.log(`لقد حصلت على جيد🙂`)
-}else if (grade > 50 && grade< 59){
+}else if (grade > 50 && grade <59 ){
 
 
 console.log(`لقد حصلت على مقبول😕`)
 
-}else if ( garde   >= 49) {
+}else if ( garde >= 49) {
 
 
     console.log(راسب)
